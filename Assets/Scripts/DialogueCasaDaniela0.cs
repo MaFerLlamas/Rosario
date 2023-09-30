7051fe5ba54361b23ca6ff687dc99b35d2fd680d
MaferLlamas <fernandaavilallamas@gmail.com>
2023-09-30 08:04:56 -0600

    Dialogo 0 casa de Daniela

--- Assets/Scripts/DialogueCasaDaniela0.cs
+++ Assets/Scripts/DialogueCasaDaniela0.cs
@@ -1,29 +1,25 @@
 using System.Collections;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using TMPro;
 
-public class DialoguePrologue : MonoBehaviour
+public class DialogueCasaDaniela0 : MonoBehaviour
 {
     [SerializeField] private GameObject dialogueMark;
     [SerializeField] private GameObject dialoguePanel;
     [SerializeField] private TMP_Text dialogueText;
-    private string targetSceneName = "PrologoExitCut";
-
-
     [SerializeField, TextArea(4,6)] private string[] dialogueLines;
 
     private float typingTime = 0.05f;
     private bool isPlayerInRange;
     private bool didDialogueStart;
-    private bool didDialogueEnd;
+    private bool firstDialogueActivated;
     private int lineIndex;
 
     // Update is called once per frame
     void Update()
     {
         if (isPlayerInRange && Input.GetButtonDown("Fire1")){
-            if (!didDialogueStart && !didDialogueEnd){
+            if (!didDialogueStart){
                 startDialogue();
             }else if (dialogueText.text == dialogueLines[lineIndex]){
                 nextDialogueLine();
@@ -47,29 +43,11 @@ public class DialoguePrologue : MonoBehaviour
         if (lineIndex < dialogueLines.Length){
             StartCoroutine(ShowLine());
         }else {
-            //END OF CONVERSATION 
             didDialogueStart = false;
-            didDialogueEnd = true;
             dialoguePanel.SetActive(false);
             dialogueMark.SetActive(true);
             Time.timeScale = 1f;
-            FindObjectOfType<MorganController>().morganShouldMove = false;
-            LoadTargetScene();   
         }
-    }
-
-    public void LoadTargetScene()
-    {
-        // Store the name of the current scene.
-        //string currentSceneName = SceneManager.GetActiveScene().name;
-
-        //Destruir antigua Daniela para que funcione la animacion
-        //Destroy(GameObject.Find("DanielaTempPrologue"));
-        // Load the target scene.
-        SceneManager.LoadScene(targetSceneName);
-
-        // Store the name of the scene to return to.
-        //PlayerPrefs.SetString("PreviousScene", currentSceneName);
     }
 
     private IEnumerator ShowLine(){
@@ -83,6 +61,8 @@ public class DialoguePrologue : MonoBehaviour
         if (collision.gameObject.CompareTag("Player")){
             isPlayerInRange = true;
             dialogueMark.SetActive(true);
+            FindObjectOfType<MorganController>().morganShouldMove = false;
+            startDialogue();
         }
     }
     private void OnTriggerExit2D(Collider2D collision){
@@ -92,3 +72,5 @@ public class DialoguePrologue : MonoBehaviour
         }
     }
 }
+
+
