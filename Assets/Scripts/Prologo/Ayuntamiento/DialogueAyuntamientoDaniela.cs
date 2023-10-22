3c77c66a90416c09584b2e686fec73a85833f87f
MaferLlamas <fernandaavilallamas@gmail.com>
2023-10-22 13:50:42 -0600

    Solving Merge problems

--- Assets/Scripts/Prologo/Ayuntamiento/DialogueAyuntamientoDaniela.cs
+++ Assets/Scripts/Prologo/Ayuntamiento/DialogueAyuntamientoDaniela.cs
@@ -22,6 +22,7 @@ public class DialogueAyuntamientoDaniela : MonoBehaviour
     void Update()
     {
         if (isPlayerInRange && Input.GetButtonDown("Fire1")){
+            Debug.Log("All is wwell");
             if (!didDialogueStart){
                 startDialogue();
             }else if (dialogueText.text == dialogue[lineIndex]){
@@ -66,6 +67,7 @@ public class DialogueAyuntamientoDaniela : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision){
         if (collision.gameObject.CompareTag("Player")){
             isPlayerInRange = true;
+            Debug.Log("hello world");
             dialogueMark.SetActive(true);
         }
     }
