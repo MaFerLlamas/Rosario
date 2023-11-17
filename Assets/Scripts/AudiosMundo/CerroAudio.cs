865d261911ccd144ea364955c6f79fcef71ba4f4
Gozhua <135038787+Gozhua@users.noreply.github.com>
2023-11-16 22:55:05 -0600

    Sonido a mundo abierto y colliders
    Sonido en el mundo abierto mediante collider is trigger, pero surgió un bug de que al entrar a la escena se reproducen todos los sonidos a la vez. Y tambiém agregue colliders a los onjetos del mundo abierto.

---
+++ Assets/Scripts/AudiosMundo/CerroAudio.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class CerroAudio : MonoBehaviour
+{
+    public AudioSource audioSource;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // Reproducir audio solo si no está sonando
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+                Debug.Log("Audio Reproducido");
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // Detener audio
+            audioSource.Stop();
+            Debug.Log("Audio Detenido");
+        }
+    }
+
+    void Start()
+    {
+        // Reproducir audio si el objeto de la zona está activo al inicio de la escena
+        if (gameObject.activeSelf)
+        {
+            audioSource.Play();
+        }
+    }
+}
