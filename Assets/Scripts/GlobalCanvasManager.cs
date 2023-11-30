03756ca235bc16fa213a8cd4d6cc53728b19bc9d
MaferLlamas <fernandaavilallamas@gmail.com>
2023-11-30 14:02:44 -0600

    Agregando el Menu

--- Assets/Scripts/GlobalCanvasManager.cs
+++ Assets/Scripts/GlobalCanvasManager.cs
@@ -1,9 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GlobalCanvasManager : MonoBehaviour
 {
     [SerializeField] public GameObject scrollView; 
+    [SerializeField] public GameObject btnOpciones; 
+    [SerializeField] public GameObject panelGlobal; 
+
+    public void Reiniciar()
+    {
+        //  Destroy(Morgan);
+        //  Destroy(Camera);
+        //  Destroy(canvas);
+        SceneManager.LoadScene("Prologo1");
+    }
+
+    public void ShowOptions()
+    {
+        btnOpciones.SetActive(false);
+        panelGlobal.SetActive(true);
+    }
+    
+    public void Continuar()
+    {
+        btnOpciones.SetActive(true);
+        panelGlobal.SetActive(false);
+    }
+
+    public void Salir()
+    {
+
+        Application.Quit();
+
+    }
   
 }
