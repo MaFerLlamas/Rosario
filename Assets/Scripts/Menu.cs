fc71132a2bfa8a276f73c05ce0254ef79022a22e
alvaromissael1@gmail.com <alvaromissael1@gmail.com>
2023-11-30 18:45:06 -0600

    agregado el restart

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -9,7 +9,24 @@ public class Menu : MonoBehaviour
     //Sirve para que nos lleve a un nuevo nivel
     public void Iniciar()
     {
+        GameObject objectEnvironment = GameObject.Find("Environment");
+        if(objectEnvironment!=null) Destroy(objectEnvironment);
+
+        GameObject objectMorgan = GameObject.Find("Morgan");
+        if (objectMorgan != null) Destroy(objectMorgan);
+
+        GameObject objectCamera = GameObject.Find("Main Camera");
+        if (objectCamera != null) Destroy(objectCamera);
+
+        GameObject objectCanvas = GameObject.Find("GlobalCanvas");
+        if (objectCanvas != null) Destroy(objectCanvas);
+
+        GameObject objectManager = GameObject.Find("QuestManager");
+        if (objectManager != null) Destroy(objectManager);
+
+        
         SceneManager.LoadScene("Prologo1");
+
     }
 
     public void Salir()
